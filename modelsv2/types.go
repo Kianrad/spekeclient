a07e16b72386b2731b7ea898605009d5c831e4bd
Kianrad <kianrad@gmail.com>
2024-08-14 16:39:03 +0200

    fix: model v2

--- modelsv2/types.go
+++ modelsv2/types.go
@@ -4,15 +4,17 @@ import "encoding/xml"
 
 // CPIXRequest represents the CPIX XML request structure
 type CPIXRequest struct {
-	XMLName        xml.Name       `xml:"cpix:CPIX"`
-	ID             string         `xml:"id,attr"`
-	XMLNSCPIX      string         `xml:"xmlns:cpix,attr"`
-	XMLNSPSKC      string         `xml:"xmlns:pskc,attr"`
-	XMLNSSPEKE     string         `xml:"xmlns:speke,attr"`
-	XMLNSDS        string         `xml:"xmlns:ds,attr"`
-	XMLNSENC       string         `xml:"xmlns:enc,attr"`
-	ContentKeyList ContentKeyList `xml:"cpix:ContentKeyList"`
-	DRMSystemList  DRMSystemList  `xml:"cpix:DRMSystemList"`
+	ContentID               string                  `xml:"contentId,attr"`
+	Version                 string                  `xml:"version,attr"`
+	XMLName                 xml.Name                `xml:"cpix:CPIX"`
+	XMLNSCPIX               string                  `xml:"xmlns:cpix,attr"`
+	XMLNSPSKC               string                  `xml:"xmlns:pskc,attr"`
+	XMLNSDS                 string                  `xml:"xmlns:ds,attr"`
+	XMLNSENC                string                  `xml:"xmlns:enc,attr"`
+	ContentKeyList          ContentKeyList          `xml:"cpix:ContentKeyList"`
+	DRMSystemList           DRMSystemList           `xml:"cpix:DRMSystemList"`
+	ContentKeyPeriodList    ContentKeyPeriodList    `xml:"cpix:ContentKeyPeriodList"`
+	ContentKeyUsageRuleList ContentKeyUsageRuleList `xml:"cpix:ContentKeyUsageRuleList"`
 }
 
 type ContentKeyList struct {
@@ -20,7 +22,9 @@ type ContentKeyList struct {
 }
 
 type ContentKey struct {
-	KID string `xml:"kid,attr"`
+	ExplicitIV             string `xml:"explicitIV,attr"`
+	KID                    string `xml:"kid,attr"`
+	CommonEncryptionScheme string `xml:"commonEncryptionScheme,attr"`
 }
 
 type DRMSystemList struct {
@@ -28,54 +32,127 @@ type DRMSystemList struct {
 }
 
 type DRMSystem struct {
-	KID                   string `xml:"kid,attr"`
-	SystemID              string `xml:"systemId,attr"`
-	ContentProtectionData string `xml:"cpix:ContentProtectionData"`
-	ProtectionHeader      string `xml:"speke:ProtectionHeader"`
-	PSSH                  string `xml:"cpix:PSSH"`
-	URIExtXKey            string `xml:"cpix:URIExtXKey"`
-	KeyFormat             string `xml:"speke:KeyFormat"`
-	KeyFormatVersions     string `xml:"speke:KeyFormatVersions"`
+	KID                                 string                               `xml:"kid,attr"`
+	SystemID                            string                               `xml:"systemId,attr"`
+	PSSH                                string                               `xml:"cpix:PSSH"`
+	ContentProtectionData               *ContentProtectionData               `xml:"cpix:ContentProtectionData,omitempty"`
+	HLSSignalingData                    []HLSSignalingData                   `xml:"cpix:HLSSignalingData"`
+	SmoothStreamingProtectionHeaderData *SmoothStreamingProtectionHeaderData `xml:"cpix:SmoothStreamingProtectionHeaderData,omitempty"`
 }
 
-////
+type HLSSignalingData struct {
+	Playlist string `xml:"playlist,attr"`
+}
+
+type PSSH struct{}
+
+type ContentProtectionData struct{}
+
+type SmoothStreamingProtectionHeaderData struct{}
+
+type ContentKeyPeriodList struct {
+	ContentKeyPeriods []ContentKeyPeriod `xml:"cpix:ContentKeyPeriod"`
+}
+
+type ContentKeyPeriod struct {
+	ID    string `xml:"id,attr"`
+	Index string `xml:"index,attr"`
+}
+
+type ContentKeyUsageRuleList struct {
+	ContentKeyUsageRules []ContentKeyUsageRule `xml:"cpix:ContentKeyUsageRule"`
+}
+
+type ContentKeyUsageRule struct {
+	KID               string          `xml:"kid,attr"`
+	IntendedTrackType string          `xml:"intendedTrackType,attr"`
+	KeyPeriodFilter   KeyPeriodFilter `xml:"cpix:KeyPeriodFilter"`
+	VideoFilter       *VideoFilter    `xml:"cpix:VideoFilter,omitempty"`
+	AudioFilter       *AudioFilter    `xml:"cpix:AudioFilter,omitempty"`
+}
+
+type KeyPeriodFilter struct {
+	PeriodID string `xml:"periodId,attr"`
+}
+
+type VideoFilter struct{}
+
+type AudioFilter struct{}
 
 type CPIXResponse struct {
-	XMLName        xml.Name               `xml:"cpix:CPIX"`
-	ID             string                 `xml:"id,attr"`
-	ContentKeyList ContentKeyListResponse `xml:"cpix:ContentKeyList"`
-	DRMSystemList  DRMSystemListResponse  `xml:"cpix:DRMSystemList"`
+	XMLName                 xml.Name                        `xml:"cpix:CPIX"`
+	ContentID               string                          `xml:"contentId,attr"`
+	Version                 string                          `xml:"version,attr"`
+	ContentKeyList          ResponseContentKeyList          `xml:"cpix:ContentKeyList"`
+	DRMSystemList           ResponseDRMSystemList           `xml:"cpix:DRMSystemList"`
+	ContentKeyPeriodList    ResponseContentKeyPeriodList    `xml:"cpix:ContentKeyPeriodList"`
+	ContentKeyUsageRuleList ResponseContentKeyUsageRuleList `xml:"cpix:ContentKeyUsageRuleList"`
 }
 
-type ContentKeyListResponse struct {
-	ContentKeys []ContentKeyResponse `xml:"cpix:ContentKey"`
+type ResponseContentKeyList struct {
+	ContentKeys []ContentKey `xml:"cpix:ContentKey"`
 }
 
-type ContentKeyResponse struct {
-	KID        string       `xml:"kid,attr"`
-	ExplicitIV string       `xml:"explicitIV,attr,omitempty"`
-	Data       DataResponse `xml:"cpix:Data"`
+type ResponseContentKey struct {
+	KID                    string       `xml:"kid,attr"`
+	ExplicitIV             string       `xml:"explicitIV,attr"`
+	CommonEncryptionScheme string       `xml:"commonEncryptionScheme,attr"`
+	Data                   ResponseData `xml:"cpix:Data"`
 }
 
-type DataResponse struct {
-	Secret SecretResponse `xml:"pskc:Secret"`
+type ResponseData struct {
+	Secret ResponseSecret `xml:"pskc:Secret"`
 }
 
-type SecretResponse struct {
+type ResponseSecret struct {
 	PlainValue string `xml:"pskc:PlainValue"`
 }
 
-type DRMSystemListResponse struct {
-	DRMSystems []DRMSystemResponse `xml:"cpix:DRMSystem"`
+type ResponseDRMSystemList struct {
+	DRMSystems []DRMSystem `xml:"cpix:DRMSystem"`
 }
 
-type DRMSystemResponse struct {
-	KID                   string `xml:"kid,attr"`
-	SystemID              string `xml:"systemId,attr"`
-	PSSH                  string `xml:"cpix:PSSH,omitempty"`
-	ContentProtectionData string `xml:"cpix:ContentProtectionData,omitempty"`
-	URIExtXKey            string `xml:"cpix:URIExtXKey,omitempty"`
-	ProtectionHeader      string `xml:"speke:ProtectionHeader,omitempty"`
-	KeyFormat             string `xml:"speke:KeyFormat,omitempty"`
-	KeyFormatVersions     string `xml:"speke:KeyFormatVersions,omitempty"`
+type ResponseDRMSystem struct {
+	SystemID              string                     `xml:"systemId,attr"`
+	KID                   string                     `xml:"kid,attr"`
+	PSSH                  string                     `xml:"cpix:PSSH"`
+	ContentProtectionData string                     `xml:"cpix:ContentProtectionData"`
+	HLSSignalingData      []ResponseHLSSignalingData `xml:"cpix:HLSSignalingData"`
+	SmoothStreamingData   string                     `xml:"cpix:SmoothStreamingProtectionHeaderData"`
+}
+
+type ResponseHLSSignalingData struct {
+	Playlist string `xml:"playlist,attr"`
+	Value    string `xml:",chardata"`
+}
+
+type ResponseContentKeyPeriodList struct {
+	ContentKeyPeriods []ResponseContentKeyPeriod `xml:"cpix:ContentKeyPeriod"`
+}
+
+type ResponseContentKeyPeriod struct {
+	ID    string `xml:"id,attr"`
+	Index string `xml:"index,attr"`
+}
+
+type ResponseContentKeyUsageRuleList struct {
+	ContentKeyUsageRules []ResponseContentKeyUsageRule `xml:"cpix:ContentKeyUsageRule"`
+}
+
+type ResponseContentKeyUsageRule struct {
+	KID               string                  `xml:"kid,attr"`
+	IntendedTrackType string                  `xml:"intendedTrackType,attr"`
+	KeyPeriodFilter   ResponseKeyPeriodFilter `xml:"cpix:KeyPeriodFilter"`
+	VideoFilter       ResponseVideoFilter     `xml:"cpix:VideoFilter"`
+	AudioFilter       ResponseAudioFilter     `xml:"cpix:AudioFilter"`
+}
+
+type ResponseKeyPeriodFilter struct {
+	PeriodID string `xml:"periodId,attr"`
+}
+
+type ResponseVideoFilter struct {
+}
+
+type ResponseAudioFilter struct {
 }
