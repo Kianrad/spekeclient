507209785b6f2b8892c2e895a2f3cc8f4f201368
Kianrad <kianrad@gmail.com>
2024-08-14 16:36:14 +0200

    fix: model v2 bug

--- client/client.go
+++ client/client.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 
 	models "github.com/Kianrad/spekeclient/models"
+	modelsv2 "github.com/Kianrad/spekeclient/modelsv2"
 )
 
 func RequestKeys(endpoint string, requestPayload models.CPIXRequest, requestHeaders map[string][]string) (*models.CPIXResponse, error) {
@@ -49,9 +50,56 @@ func RequestKeys(endpoint string, requestPayload models.CPIXRequest, requestHead
 		fmt.Println("Response body:", string(responseBody))
 		return nil, err
 	}
-
 	decoder := nxml.NewDecoder(bytes.NewReader(responseBody))
+
 	var spekeResponse models.CPIXResponse
+	err = decoder.Decode(&spekeResponse)
+	if err != nil {
+		fmt.Println("Error unmarshaling response XML:", err)
+		return nil, err
+	}
+	return &spekeResponse, nil
+}
+
+func RequestV2Keys(endpoint string, requestPayload modelsv2.CPIXRequest, requestHeaders map[string][]string) (*modelsv2.CPIXResponse, error) {
+
+	requestBody, err := xml.Marshal(requestPayload)
+	if err != nil {
+		fmt.Println("Error marshaling request payload:", err)
+		return nil, err
+	}
+
+	requestBody = []byte(xml.Header + string(requestBody))
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println("Error creating HTTP request:", err)
+		return nil, err
+	}
+
+	req.Header = requestHeaders
+	req.Header.Set("X-Speke-Version", "2.0")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending HTTP request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: received non-200 status code:", resp.StatusCode)
+		fmt.Println("Response body:", string(responseBody))
+		return nil, err
+	}
+	decoder := nxml.NewDecoder(bytes.NewReader(responseBody))
+	var spekeResponse modelsv2.CPIXResponse
 	err = decoder.Decode(&spekeResponse)
 	if err != nil {
 		fmt.Println("Error unmarshaling response XML:", err)
